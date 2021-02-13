d364c2248cbe145db0d9e8a23e4d4c860d20e6a4
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-02-12 21:48:57 -0300

    feat(migration) Criação da tabela categoria e as  relações das tabelas estabelecimento e categoria feita!

---
+++ src/shared/typeorm/migrations/1613176556129-CreateCategoryEstablishment.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategoryEstablishment1613176556129
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'category_establishment',
+
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('category_establishment');
+  }
+}
---
+++ src/shared/typeorm/migrations/1613176916870-AddCategoryIdToEstablishment.ts
@@ -0,0 +1,34 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddCategoryIdToEstablishment1613176916870
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'establishment',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'establishment',
+      new TableForeignKey({
+        name: 'CategoryEetablishment',
+        columnNames: ['category_id'],
+        referencedTableName: 'category_establishment',
+        referencedColumnNames: ['id'],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('establishment', 'CategoryEetablishment');
+    await queryRunner.dropColumn('establishment', 'category_id');
+  }
+}
