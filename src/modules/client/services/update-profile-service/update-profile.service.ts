ce64be75ec2c16e938cdef4e536e58df4d4c00c7
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-02-17 13:39:34 -0300

    feat(estabelecimento) serviço de update criado

--- src/modules/client/services/update-profile-service/update-profile.service.ts
+++ src/modules/client/services/update-profile-service/update-profile.service.ts
@@ -1,13 +1,21 @@
 /**
+
  * @fileoverview Criação do serviço para atualização do perfil do usuário
+
  *
+
  * @author Bruno Mesquita
+
  */
+
 import { getCustomRepository } from 'typeorm';
 
 import { ServiceResponse } from '@shared/utils/service-response';
+
 import ClientRepository from '../../typeorm/repository';
+
 import { UpdateClientDto } from '../../dtos/update-client-dto';
+
 import updateClientValidation from '../../validation/update-client.validation';
 
 class UpdateProfileService {
@@ -16,11 +24,13 @@ class UpdateProfileService {
       const clientRepository = getCustomRepository(ClientRepository);
 
       // validando dto
+
       const valid = updateClientValidation.isValidSync(updateClientDto);
 
       if (!valid) throw new Error('Dados inválidos');
 
       // verificando se o usuário existe
+
       const user = await clientRepository.findById(updateClientDto.id);
 
       if (!user) throw new Error('Usuário não encontrado');
