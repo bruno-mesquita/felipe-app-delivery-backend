49df48bf5fc08bea4aea13d9e17f4d5498fb467d
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-02-20 17:05:53 -0300

    feat(auth login) controller, feito.

---
+++ src/store/modules/auth/auth-login-controller.ts
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview Controller da sessão do Estabelecimento
+ *
+ * @author Jonatas Rosa Moura
+ */
+
+import { Request, Response } from 'express';
+import { LoginEstablishmentLoginService } from './services/login-service/login-establishment.service';
+
+export class SessionsController {
+  async create(req: Request, res: Response): Promise<Response> {
+    try {
+      const loginService = new LoginEstablishmentLoginService();
+
+      const result = await loginService.execute(req.body);
+
+      if (result.err) throw new Error(result.err);
+
+      return res.json(result.result);
+    } catch (err) {
+      return res.status(401).json({ err: err.message });
+    }
+  }
+}
