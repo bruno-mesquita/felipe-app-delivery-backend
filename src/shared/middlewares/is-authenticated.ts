0f2f9335da94ddef32d9e008f8f06442af9c77ed
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-03-19 21:35:51 -0300

    fix #Middleware de autenticaçao

--- src/shared/middlewares/is-authenticated.ts
+++ src/shared/middlewares/is-authenticated.ts
@@ -1,29 +1,33 @@
 /**
+
  * @fileoverview verificar se o usuário está autenticado -> middleware de autenticação -> proteger as rotas que vão ser verificadas
+
  * @author Jonatas Rosa Moura
+
  */
 
 import { Request, Response, NextFunction } from 'express';
 
 import TokenManager from '@shared/utils/token-manager';
 
-export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void {
-  const authConfig = new TokenManager();
-  // verificar se no cabeçalho existe um token
-
-  const authHeader = request.headers.authorization;
-
-  if (!authHeader) {
-    throw new Error('[Falha de autenticação]: JWT Token está ausente.');
-  }
-
-  // array token vai ter 2 posições -> Bearer açsd6464JAFPO(&&%/;). -> chave secreta (token)
-
-  const [, token] = authHeader.split(' ');
-
-  // verificar se esse token foi criado pela minha aplicação, se ele é válido (se eu posso liberar o acesso)
-
+export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void | Response {
   try {
+    const authConfig = new TokenManager();
+
+    // verificar se no cabeçalho existe um token
+
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return response.status(401).json('[Falha de autenticação]: JWT Token está ausente');
+    }
+
+    // array token vai ter 2 posições -> Bearer açsd6464JAFPO(&&%/;). -> chave secreta (token)
+
+    const [, token] = authHeader.split(' ');
+
+    // verificar se esse token foi criado pela minha aplicação, se ele é válido (se eu posso liberar o acesso)
+
     const decodedToken = authConfig.check(token);
 
     request.client = {
