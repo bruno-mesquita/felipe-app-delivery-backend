cd2f66dd75ba67b34b8c939ecc49defca5771fde
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-03-22 22:51:49 -0300

    feat(Avatar) #Configurando avatar no CreateClientService

--- src/customer/modules/client/services/create-client-service/create-client.service.ts
+++ src/customer/modules/client/services/create-client-service/create-client.service.ts
@@ -9,6 +9,7 @@ import { getCustomRepository } from 'typeorm';
 import SmsService from '@shared/utils/sms';
 
 import { ServiceResponse } from '@shared/utils/service-response';
+import { id } from 'date-fns/locale';
 import { ClientActivationCodeRepository } from '../../../client-activation-code';
 import { AddressClientRepository } from '../../../address-client';
 import { AddressRepository } from '../../../address';
@@ -16,6 +17,7 @@ import { CityRepository } from '../../../city';
 import UserRepository from '../../client.repository';
 import { CreateClientDto } from '../../dtos/create-client-dto';
 import createClientSchema from '../../validation/create-client.validation';
+import { AvatarRepository } from '../../../avatar/avatar-repository';
 
 class CreateClientService {
   async execute(createClientDto: CreateClientDto): Promise<ServiceResponse<string | null>> {
@@ -26,6 +28,7 @@ class CreateClientService {
       const addressRepository = getCustomRepository(AddressRepository);
       const addressClientRepository = getCustomRepository(AddressClientRepository);
       const clientActivationCodeRepository = getCustomRepository(ClientActivationCodeRepository);
+      const avatarRepository = getCustomRepository(AvatarRepository);
 
       // Verificando se as senhas são iguais
       if (createClientDto.confirmPassword !== createClientDto.password) throw new Error('Senhas não são iguais');
@@ -38,15 +41,26 @@ class CreateClientService {
       // Verificando se já existe um cliente com esse cpf
       const userCpfExists = await userRepository.findByCpf(createClientDto.cpf);
 
-      if (userCpfExists) throw new Error('Já existe um cliente cadastrado com esse cpf');
+      if (userCpfExists) throw new Error('Já existe um usuário cadastrado com esse cpf');
 
       // Verificando se já existe um cliente com esse email
       const userEmailExists = await userRepository.findByEmail(createClientDto.email);
 
-      if (userEmailExists) throw new Error('Já existe um cliente cadastrado com esse email');
+      if (userEmailExists) throw new Error('Já existe um usuário cadastrado com esse email');
+
+      // Verificando se já existe um cliente com esse numero
+      const userCellphoneExists = await userRepository.findByCellphone(createClientDto.cellphone);
+
+      if (userCellphoneExists) throw new Error('Já existe um usuário cadastrado com esse Número de Celular');
+
+      // Avatar
+      const userAvatar = await avatarRepository.findOne({
+        where: id,
+        relations: ['image_id'],
+      });
 
       // Criando a classe
-      const user = userRepository.create(createClientDto);
+      const user = userRepository.create({ ...createClientDto, image: userAvatar });
 
       // Salvando no db
       await userRepository.save(user);
