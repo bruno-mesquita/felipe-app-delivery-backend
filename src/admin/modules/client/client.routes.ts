910bb0baf5c2a431381b841dedd6600b9d755982
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-04-02 23:31:02 -0300

    feat(sequelize) service de criaçao de cidade

--- src/admin/modules/client/client.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ClientsAdminController from './controllers/clients.controller';
-
-const clientRouterAdmin = Router();
-const clientAdminController = new ClientsAdminController();
-
-clientRouterAdmin.get('/list', clientAdminController.list);
-
-export { clientRouterAdmin };
