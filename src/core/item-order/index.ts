7050b37a3e52e060536225ac227c7641f6c7d892
Bruno Mesquita <silvabruno.git@gmail.com>
2021-04-07 20:01:38 -0300

    #fix relacionamento de itemOrder

--- src/core/item-order/index.ts
+++ src/core/item-order/index.ts
@@ -5,16 +5,21 @@ import Model from '../_Bases/model';
 class ItemOrder extends Model {
   quantity: number;
   total: number;
+  product_id: string;
+  order_id: string;
 
   static start(sequelize: Sequelize) {
     this.init({
       quantity: DataTypes.NUMBER,
       total: DataTypes.NUMBER,
-      productId: DataTypes.STRING,
-      orderId: DataTypes.STRING,
     }, { sequelize, tableName: 'item_order' });
 
     return this;
+  }
+
+  static associate({ Product, Order }): void {
+    this.belongsTo(Product, { foreignKey: 'product_id', as: 'product' })
+    this.belongsTo(Order, { foreignKey: 'order_id', as: 'order' })
   }
 }
 
