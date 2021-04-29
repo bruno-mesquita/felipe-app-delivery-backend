0e4e2ab203afb142754c58b8f7d78bca53b22076
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-04-29 19:49:31 -0300

    feat(criaçao da migration de Termos e Uso)

---
+++ src/shared/database/migrations/20210429224259-create-terms-use.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('terms-of-use', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT(),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('terms-of-use');
+  }
+};
