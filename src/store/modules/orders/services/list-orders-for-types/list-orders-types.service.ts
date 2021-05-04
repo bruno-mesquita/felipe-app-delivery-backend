a7f6b2a01e1c6b721d01cd2103d2296619703e7d
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-05-03 21:17:40 -0300

    fix(listar os pedidos)

--- src/store/modules/orders/services/list-orders-for-types/list-orders-types.service.ts
+++ src/store/modules/orders/services/list-orders-for-types/list-orders-types.service.ts
@@ -5,6 +5,8 @@ import Establishment from "@core/establishment";
 import Order from "@core/order";
 import { ServiceResponse } from "@shared/utils/service-response";
 import { ListOrdersDto } from '../../dtos/list-orders-types.dto';
+import State from "@core/state";
+import City from "@core/city";
 
 export class ListOrdersForTypesServices {
   static LIMIT = 15;
@@ -20,15 +22,30 @@ export class ListOrdersForTypesServices {
 
       const orders = await establishment.getOrders({
         where: {
+          address_id: { [Op.ne]: null },
           payment: 'Dinheiro' || 'Cartão de crédito' || 'Cartão de débidto',
           order_status: 'Aberto' || 'Em andamento' || 'Finalizado' || 'Cancelado',
-          address_id: { [Op.ne]: null },
         },
+        attributes: ['payment', 'order_status', 'createdAt'],
         include: [
           {
             model: AddressClient,
             as: 'address_client',
             attributes: ['id', 'client_id', 'nickname', 'street', 'number', 'neighborhood', 'cep', 'city_id'],
+            include: [
+              {
+                model: City,
+                as: 'city',
+                attributes: ['id' ,'name'],
+                include: [
+                  {
+                    model: State,
+                    as: 'state',
+                    attributes: ['id', 'name'],
+                  },
+                ],
+              },
+            ],
           },
         ],
         limit,
