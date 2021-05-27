f698c2b15ff2eb7bd523fa84e9ff13cf0ee978d6
Bruno Mesquita <silvabruno.git@gmail.com>
2021-05-26 23:26:06 -0300

    #fix autenticação pelo id do dono do estabelecimento

--- src/shared/middlewares/access-establishment.ts
+++ src/shared/middlewares/access-establishment.ts
@@ -2,15 +2,15 @@
  * @fileoverview Verificando se é o id do admin que está criando nessa rota
  */
 
- import Establishment from '@core/establishment';
+import { EstablishmentOwner } from '@core/establishment-owner';
  import { Request, Response, NextFunction } from 'express';
 
  export async function accessEstablishment(request: Request, response: Response, next: NextFunction): Promise<Response | void> {
    try {
     // Verificar o id do admin
-    const storeId = request.client.id;
+    const ownerId = request.client.id;
 
-    const store = await Establishment.findByPk(storeId);
+    const store = await EstablishmentOwner.findByPk(ownerId);
 
     if (!store) {
       return response.status(401).json('[Acesso]: ID está ausente');
