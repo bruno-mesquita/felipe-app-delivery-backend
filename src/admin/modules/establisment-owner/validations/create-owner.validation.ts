ca9621c079e056d6d0e437c545e5ccb310d64b22
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-06-03 20:11:32 -0300

    fix(renovendo a formataçao do telefone em validation)

--- src/admin/modules/establisment-owner/validations/create-owner.validation.ts
+++ src/admin/modules/establisment-owner/validations/create-owner.validation.ts
@@ -4,14 +4,12 @@ import { CreateOwnerDto } from '../dtos/create-owner-dtos';
 
 const REQUIRED = 'Campo obrigátorio';
 
-const phoneRegxp = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/;
-
 const schema: SchemaOf<CreateOwnerDto> = object({
   firstName: string().required(REQUIRED),
   lastName: string().required(REQUIRED),
   email: string().email().required(REQUIRED),
   password: string().required(REQUIRED),
-  cellphone: string().required(REQUIRED).matches(phoneRegxp, 'Telefone inválido'),
+  cellphone: string().required(REQUIRED),
   active: boolean().required(REQUIRED),
   cpf: string().required(REQUIRED),
 });
