c941287c90aba7865eab99b94b2fef876e828f2a
Bruno Mesquita <silvabruno.git@gmail.com>
2021-06-09 21:41:06 -0300

    #fix rota de avaliações sem autenticação

--- src/store/modules/rate/rate.routes.ts
+++ src/store/modules/rate/rate.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import { RateController } from './rate.controller';
+import { accessEstablishmentOwner } from '@shared/middlewares/access-establishment-owner';
+import isAuthenticated from '@shared/middlewares/is-authenticated';
 
 const routes = Router();
 const rateController = new RateController();
 
-routes.get('/list-rates', rateController.listRates);
+routes.get('/list-rates', isAuthenticated, accessEstablishmentOwner, rateController.listRates);
 
 export default routes;
