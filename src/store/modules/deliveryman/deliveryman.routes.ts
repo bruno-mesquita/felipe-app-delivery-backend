eda3fdfdebb6abdaf4bd99e32587fc81f9a0dae5
Bruno Mesquita <silvabruno.git@gmail.com>
2021-06-14 21:39:01 -0300

    #fix autenticação rotas de motoboys

--- src/store/modules/deliveryman/deliveryman.routes.ts
+++ src/store/modules/deliveryman/deliveryman.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import { accessEstablishmentOwner } from '@shared/middlewares/access-establishment-owner';
+import isAuthenticated from '@shared/middlewares/is-authenticated';
 import { DeliverymanController } from './deliveryman.controller';
 
 const routes = Router();
 const deliverymanController = new DeliverymanController();
 
-routes.get('/deliverymen', accessEstablishmentOwner, deliverymanController.list);
+routes.get('/deliverymen', isAuthenticated, accessEstablishmentOwner, deliverymanController.list);
 
 export default routes;
