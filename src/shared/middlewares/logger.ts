cd371c51590cb7fc1de9e0128e28f58a69078496
Bruno Mesquita <silvabruno.git@gmail.com>
2022-02-08 00:03:59 -0300

    #chore logger somente acima ou igual 400

--- src/shared/middlewares/logger.ts
+++ src/shared/middlewares/logger.ts
@@ -1,36 +1,38 @@
-/**
- * @fileoverview Middleware para armazenar os logs
- */
-
+import type { IncomingMessage } from 'http';
+import type { Request } from 'express';
 import morgan from 'morgan';
-import { resolve,  } from 'path';
+import { resolve } from 'path';
 import { existsSync, mkdirSync } from 'fs';
 import { format } from 'date-fns';
 
 import JsonHandler from '../utils/json-handler';
 
-const getIp = (req: any) => {
+const getIp = (req: IncomingMessage & Request) => {
   return req.ip || req.ips || (req.socket && req.socket.remoteAddress) || undefined;
 };
 
 const loggerMiddleware = () => {
   return morgan((tokens, req, res) => {
-    const pathRoot = resolve(__dirname, '..', '..', '..', 'logs');
+    const status = tokens.status(req, res);
 
-    if (!existsSync(pathRoot)) mkdirSync(pathRoot);
+    if(Number(status) >= 400) {
+      const pathRoot = resolve(__dirname, '..', '..', '..', 'logs');
 
-    const jsonHandler = new JsonHandler(resolve(pathRoot, `requests-${tokens.status(req, res)}.json`));
-    const result = {
-      ip: getIp(req as any),
-      method: tokens.method(req, res),
-      url: tokens.url(req, res),
-      status: tokens.status(req, res),
-      contentLength: tokens.res(req, res, 'content-length'),
-      responseTime: `${tokens['response-time'](req, res)} ms`,
-      date: format(new Date(), 'dd/mm/yyyy HH:mm:MM'),
-    };
-
-    jsonHandler.add(result);
+      if (!existsSync(pathRoot)) mkdirSync(pathRoot);
+  
+      const jsonHandler = new JsonHandler(resolve(pathRoot, `requests-${tokens.status(req, res)}.json`));
+      const result = {
+        ip: getIp(req as any),
+        method: tokens.method(req, res),
+        url: tokens.url(req, res),
+        status: tokens.status(req, res),
+        contentLength: tokens.res(req, res, 'content-length'),
+        responseTime: `${tokens['response-time'](req, res)} ms`,
+        date: format(new Date(), 'dd/MM/yyyy HH:mm'),
+      };
+  
+      jsonHandler.add(result);
+    }
 
     return '';
   });
