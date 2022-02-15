ae08627352644598f8d8903fad571a253628f522
Bruno Mesquita <silvabruno.git@gmail.com>
2022-02-15 18:20:56 -0300

    #fix update product

--- src/store/modules/product/services/update-product-service/update-product.service.ts
+++ src/store/modules/product/services/update-product-service/update-product.service.ts
@@ -3,7 +3,7 @@ import Menu from '@core/menu';
 import Product from '@core/product';
 import ApiError from '@shared/utils/ApiError';
 import { ServiceResponse } from '@shared/utils/service-response';
-import { UpdateProductDto } from '../../dtos/update-product-dto';
+import type { UpdateProductDto } from '../../dtos/update-product-dto';
 
 export class UpdateProductService {
   async execute(updateProductDto: UpdateProductDto): Promise<ServiceResponse<boolean>> {
@@ -22,14 +22,18 @@ export class UpdateProductService {
       // Editando classe e Salvando no DB
       const { name, price, description, image, active, menu } = updateProductDto;
 
-      product.updateProduct(name, price, description, menu, active);
+      
+      await product.update({
+        name,
+        price,
+        description,
+        active,
+        menu_id: menu,
+      });
 
-      const photo = await Image.findOne({ where: { id: product.get('image_id') } });
+      const photo = await Image.findOne({ where: { id: product.get('image_id') }, attributes: ['encoded'] });
 
-      photo.setEncoded(image);
-
-      await photo.save();
-      await product.save();
+      await photo.update({ encoded: image });
 
       return { result: true, err: null };
     } catch (err) {
