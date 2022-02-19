2e58600309792bd81cfc7699975bed3f3cdc61e5
Bruno Mesquita <silvabruno.git@gmail.com>
2022-02-18 23:36:58 -0300

    fix: referencia de classe bugando axiox com nginx

--- src/store/modules/notifications/services/RegisterProviderNotifcations.ts
+++ src/store/modules/notifications/services/RegisterProviderNotifcations.ts
@@ -2,15 +2,11 @@ import ApiError from '@shared/utils/ApiError';
 import Notification from '@shared/utils/Notification';
 
 export class RegisterProviderNotification {
-  private readonly notificationService: Notification;
-
-  constructor() {
-    this.notificationService = new Notification();
-  }
-
   async execute(pushToken: string, clientId: number): Promise<void> {
     try {
-      await this.notificationService.addListener({
+      const notificationService =  new Notification();
+
+      await notificationService.addListener({
         targetId: clientId,
         token: pushToken,
         type: 'Partner'
